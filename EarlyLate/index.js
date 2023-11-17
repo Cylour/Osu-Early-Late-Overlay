088b35f4a9ef73faf1f30c295ddb4bfd62e4699d
Cylour <ray.chen1309@gmail.com>
2023-11-17 18:51:00 +1100

    feat: 20 note error history bar on top

--- EarlyLate/index.js
+++ EarlyLate/index.js
@@ -27,36 +27,58 @@ const ifErrorHit = (currHits, prevHits) => {
   );
 };
 
-const setOffsetEarly = (latestError, error, error_offset) => {
+const setOffsetEarly = (latestError, error, error_offset, error_hist) => {
   error.innerHTML = `Early`;
   error_offset.innerHTML = `${-latestError}ms`;
   let errorVal = Math.min(Math.abs(latestError), 100);
   error.style.color = 'rgb(0, 128, 255)';
   error_offset.style.color = 'rgb(0, 128, 255)';
   error_offset.style.fontSize = `${0.3 + (0.5 / 100) * errorVal}rem`;
+
+  error_hist.removeChild(error_hist.firstElementChild);
+  const errorBlock = document.createElement('div');
+  errorBlock.className = 'error-early';
+  error_hist.appendChild(errorBlock);
 };
 
-const setOffsetLate = (latestError, error, error_offset) => {
+const setOffsetLate = (latestError, error, error_offset, error_hist) => {
   error.innerHTML = `Late`;
   error_offset.innerHTML = `${latestError}ms`;
   let errorVal = Math.min(Math.abs(latestError), 100);
   error.style.color = 'red';
   error_offset.style.color = 'red';
   error_offset.style.fontSize = `${0.3 + (0.5 / 100) * errorVal}rem`;
+
+  error_hist.removeChild(error_hist.firstElementChild);
+  const errorBlock = document.createElement('div');
+  errorBlock.className = 'error-late';
+  error_hist.appendChild(errorBlock);
 };
 
-function reset_animation() {
+const doResetAnimation = (element) => {
+  element.style.animation = 'none';
+  element.offsetHeight;
+  element.style.animation = null;
+};
+
+const reset_animation = () => {
   const el = document.getElementById('error');
   const el_o = document.getElementById('error-offset');
-  el.style.animation = 'none';
-  el_o.style.animation = 'none';
-  el.offsetHeight; /* trigger reflow */
-  el_o.offsetHeight;
-  el.style.animation = null;
-  el_o.style.animation = null;
-}
+  const el_h = document.getElementById('error-hist');
+  doResetAnimation(el);
+  doResetAnimation(el_o);
+  el_h.childNodes.forEach((el) => {
+    doResetAnimation(el);
+  });
+};
 
 const error = document.getElementById('error');
+const error_hist = document.getElementById('error-hist');
+for (let i = 0; i < 20; i++) {
+  const errorBlock = document.createElement('div');
+  errorBlock.className = 'error-default';
+  error_hist.appendChild(errorBlock);
+}
 const error_offset = document.getElementById('error-offset');
 
 let prevHits = null;
@@ -72,9 +94,9 @@ socket.onmessage = (event) => {
         let latestError = hitErrors[hitErrors.length - 1];
         reset_animation();
         if (latestError < 0) {
-          setOffsetEarly(latestError, error, error_offset);
+          setOffsetEarly(latestError, error, error_offset, error_hist);
         } else {
-          setOffsetLate(latestError, error, error_offset);
+          setOffsetLate(latestError, error, error_offset, error_hist);
         }
       }
       prevHits = currHits;
