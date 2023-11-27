db4f5afc03a6a74ab2bf338eb351eb55cfe30d69
Cylour <ray.chen1309@gmail.com>
2023-11-27 19:34:54 +1100

    Error combo feature along with more obvious error display

--- EarlyLate/index.js
+++ EarlyLate/index.js
@@ -27,26 +27,44 @@ const ifErrorHit = (currHits, prevHits) => {
   );
 };
 
-const setOffsetEarly = (latestError, error, error_offset, error_line) => {
+const setOffsetEarly = (
+  curr_error,
+  error,
+  err_combo_val,
+  error_combo,
+  error_offset,
+  error_line
+) => {
   error.innerHTML = `EARLY`;
-  error_offset.innerHTML = `${-latestError}ms`;
+  error_combo.innerHTML = `&times${err_combo_val}`;
+  error_offset.innerHTML = `${-curr_error}ms`;
   error.style.color = 'rgb(0, 128, 255)';
+  error_combo.style.color = 'rgb(0, 128, 255)';
   error_offset.style.color = 'rgb(0, 128, 255)';
   error_line.style.backgroundColor = 'rgb(0, 128, 255)';
-  let errorVal = Math.min(Math.abs(latestError), 100);
+  let errorVal = Math.min(Math.abs(curr_error), 100);
   error_offset.style.fontSize = `${0.3 + (0.5 / 100) * errorVal}rem`;
   error_line.style.boxShadow = `0px 0px 50px ${
     15 + (20 / 100) * errorVal
   }px rgb(0, 128, 255)`;
 };
 
-const setOffsetLate = (latestError, error, error_offset, error_line) => {
+const setOffsetLate = (
+  curr_error,
+  error,
+  err_combo_val,
+  error_combo,
+  error_offset,
+  error_line
+) => {
   error.innerHTML = `LATE`;
-  error_offset.innerHTML = `${latestError}ms`;
+  error_combo.innerHTML = `&times${err_combo_val}`;
+  error_offset.innerHTML = `${curr_error}ms`;
   error.style.color = 'red';
+  error_combo.style.color = 'red';
   error_offset.style.color = 'red';
   error_line.style.backgroundColor = 'red';
-  let errorVal = Math.min(Math.abs(latestError), 100);
+  let errorVal = Math.min(Math.abs(curr_error), 100);
   error_offset.style.fontSize = `${0.3 + (0.5 / 100) * errorVal}rem`;
   error_line.style.boxShadow = `0px 0px 50px ${
     5 + (30 / 100) * errorVal
@@ -63,36 +81,76 @@ const reset_animation = () => {
   const el = document.getElementById('error');
   const el_o = document.getElementById('error-offset');
   const el_l = document.getElementById('error-line');
+  const el_c = document.getElementById('error-combo');
   doResetAnimation(el);
   doResetAnimation(el_o);
   doResetAnimation(el_l);
+  doResetAnimation(el_c);
 };
 
 const error = document.getElementById('error');
+const error_wrapper = document.getElementById('error-wrapper');
 const error_offset = document.getElementById('error-offset');
 const error_line = document.getElementById('error-line');
+const error_combo = document.getElementById('error-combo');
 
-let prevHits = null;
-let currHits = null;
+let prev_hits = null;
+let curr_hits = null;
+let prev_error = null;
+let err_combo_val = 0;
+
 socket.onmessage = (event) => {
   try {
     const data = JSON.parse(event.data),
       hits = data.gameplay.hits;
     const hitErrors = hits.hitErrorArray;
     if (hitErrors) {
-      currHits = hits;
-      if (ifErrorHit(currHits, prevHits)) {
-        let latestError = hitErrors[hitErrors.length - 1];
+      curr_hits = hits;
+      if (ifErrorHit(curr_hits, prev_hits)) {
+        let curr_error = hitErrors[hitErrors.length - 1];
         reset_animation();
-        if (latestError < 0) {
-          setOffsetEarly(latestError, error, error_offset, error_line);
+        if (curr_error < 0) {
+          if (prev_error === null || prev_error >= 0) err_combo_val = 1;
+          else err_combo_val++;
+          err_combo_val === 1
+            ? (error_combo.style.display = 'none')
+            : (error_combo.style.display = 'block');
+          document.documentElement.style.setProperty(
+            '--combo-size',
+            1 + Math.min(err_combo_val, 10) / 10
+          );
+          setOffsetEarly(
+            curr_error,
+            error,
+            err_combo_val,
+            error_combo,
+            error_offset,
+            error_line
+          );
         } else {
-          setOffsetLate(latestError, error, error_offset, error_line);
+          if (prev_error === null || prev_error < 0) err_combo_val = 1;
+          else err_combo_val++;
+          err_combo_val === 1
+            ? (error_combo.style.display = 'none')
+            : (error_combo.style.display = 'block');
+          document.documentElement.style.setProperty(
+            '--combo-size',
+            1 + Math.min(err_combo_val, 10) / 10
+          );
+          setOffsetLate(
+            curr_error,
+            error,
+            err_combo_val,
+            error_combo,
+            error_offset,
+            error_line
+          );
         }
-      }
-      prevHits = currHits;
+        prev_error = curr_error;
+      } else err_combo_val = 0;
+      prev_hits = curr_hits;
     } else {
-      prevHits = null;
+      prev_hits = null;
     }
   } catch (err) {
     console.log(err);
