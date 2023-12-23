fd6ed214dd0217bb6983e7c95f630e2960604bf9
Cylour <ray.chen1309@gmail.com>
2023-12-23 14:47:51 +1100

    Now knows when slider ends are missed

--- EarlyLate/index.js
+++ EarlyLate/index.js
@@ -43,9 +43,9 @@ const setOffsetEarly = (
   error_offset.style.color = 'rgb(0, 128, 255)';
   error_line.style.backgroundColor = 'rgb(0, 128, 255)';
   let errorVal = Math.min(Math.abs(curr_error), 100);
-  error_offset.style.fontSize = `${0.3 + (0.5 / 100) * errorVal}rem`;
+  error_offset.style.fontSize = `${0.2 + (0.7 / 100) * errorVal}rem`;
   error_line.style.boxShadow = `0px 0px 50px ${
-    15 + (20 / 100) * errorVal
+    5 + (30 / 100) * errorVal
   }px rgb(0, 128, 255)`;
 };
 
@@ -65,10 +65,25 @@ const setOffsetLate = (
   error_offset.style.color = 'red';
   error_line.style.backgroundColor = 'red';
   let errorVal = Math.min(Math.abs(curr_error), 100);
-  error_offset.style.fontSize = `${0.3 + (0.5 / 100) * errorVal}rem`;
+  error_offset.style.fontSize = `${0.2 + (0.7 / 100) * errorVal}rem`;
   error_line.style.boxShadow = `0px 0px 50px ${
     5 + (30 / 100) * errorVal
   }px red`;
+};
+
+const setOffsetSlider = (
+  curr_error,
+  error,
+  err_combo_val,
+  error_offset,
+  error_line
+) => {
+  error.innerHTML = `MISS SLIDER END`;
+  error_combo.innerHTML = `&times${err_combo_val}`;
+  error.style.color = 'purple';
+  error_offset.innerHTML = '';
+  error_line.style.backgroundColor = 'purple';
+  error_line.style.boxShadow = `0px 0px 50px 20px purple`;
 };
 
 const doResetAnimation = (element) => {
@@ -88,37 +103,80 @@ const reset_animation = () => {
   doResetAnimation(el_c);
 };
 
+const set_combo_vals = (err_combo_val) => {
+  const err_val = Math.min(err_combo_val, 10);
+  document.documentElement.style.setProperty(
+    '--combo-size',
+    1.1 + (err_val / 10) ** 1.5
+  );
+  document.documentElement.style.setProperty(
+    '--combo-rot',
+    10 + 3 * err_val + 'deg'
+  );
+};
+
 const error = document.getElementById('error');
 const error_wrapper = document.getElementById('error-wrapper');
 const error_offset = document.getElementById('error-offset');
 const error_line = document.getElementById('error-line');
 const error_combo = document.getElementById('error-combo');
+const od_wrapper = document.getElementById('od-wrapper');
+const od_element = document.getElementById('od-ms');
 
 let prev_hits = null;
 let curr_hits = null;
 let prev_error = null;
+let prev_menu_song_id = null;
 let err_combo_val = 0;
+let od_ms = null;
 
 socket.onmessage = (event) => {
   try {
     const data = JSON.parse(event.data),
       hits = data.gameplay.hits;
     const hitErrors = hits.hitErrorArray;
+    const od = data.menu.bm.stats.OD;
+    if (data.menu.state == 2) od_wrapper.style.display = 'none';
+    else if (od) {
+      if (data.menu.bm.id !== prev_menu_song_id) doResetAnimation(od_wrapper);
+      od_wrapper.style.display = 'block';
+      od_ms = 80 - 6 * Number(od);
+      od_element.innerHTML = '±' + Math.round(od_ms * 10) / 10 + 'ms';
+      prev_menu_song_id = data.menu.bm.id;
+    } else {
+      od_wrapper.style.display = 'none';
+    }
     if (hitErrors) {
       curr_hits = hits;
       if (ifErrorHit(curr_hits, prev_hits)) {
         let curr_error = hitErrors[hitErrors.length - 1];
         reset_animation();
-        if (curr_error < 0) {
+        // super scuffed Miss slider end
+        if (Math.abs(curr_error) < od_ms) {
           if (prev_error === null || prev_error >= 0) err_combo_val = 1;
           else err_combo_val++;
           err_combo_val === 1
             ? (error_combo.style.display = 'none')
             : (error_combo.style.display = 'block');
-          document.documentElement.style.setProperty(
-            '--combo-size',
-            1 + Math.min(err_combo_val, 10) / 10
+
+          set_combo_vals(err_combo_val);
+          setOffsetSlider(
+            curr_error,
+            error,
+            err_combo_val,
+            error_offset,
+            error_line
           );
+          // if Early
+        } else if (curr_error < 0) {
+          if (prev_error === null || prev_error >= 0) err_combo_val = 1;
+          else err_combo_val++;
+          err_combo_val === 1
+            ? (error_combo.style.display = 'none')
+            : (error_combo.style.display = 'block');
+
+          set_combo_vals(err_combo_val);
+
           setOffsetEarly(
             curr_error,
             error,
@@ -127,16 +185,16 @@ socket.onmessage = (event) => {
             error_offset,
             error_line
           );
+          // if Late
         } else {
           if (prev_error === null || prev_error < 0) err_combo_val = 1;
           else err_combo_val++;
           err_combo_val === 1
             ? (error_combo.style.display = 'none')
             : (error_combo.style.display = 'block');
-          document.documentElement.style.setProperty(
-            '--combo-size',
-            1 + Math.min(err_combo_val, 10) / 10
-          );
+
+          set_combo_vals(err_combo_val);
+
           setOffsetLate(
             curr_error,
             error,
